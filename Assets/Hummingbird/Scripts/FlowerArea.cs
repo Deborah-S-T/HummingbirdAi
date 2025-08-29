6a80b0bf9ac42ebc1c9dadb32434e0887990a769
Deborah-S-T <deborahsolomon04@gmail.com>
2025-08-29 16:10:53 +0300

    variables for the flowerArea class defined

--- Assets/Hummingbird/Scripts/FlowerArea.cs
+++ Assets/Hummingbird/Scripts/FlowerArea.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 
+/// <summary>
+/// Manages a collection of flower plants and attached flowers
+/// </summary>
 public class FlowerArea : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // The diameter of the area where the agent and flower can be
+    // used for observing relative distance from agent to flower
+    public const float AreaDiameter = 20f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // The list of all flower plants in this area (flower plants have multiple flowers)
+    private List<GameObject> flowerPlants;
+
+    // A lookup dictionnary for looking up a flower from a nectar collider
+    private Dictionnary<Collider, Flower> nectarFlowerDictionnary;
+
+    /// <summary>
+    /// The list of all flowers in the flower area
+    /// </summary>
+    public List<Flower> Flowers { get; private set; }
 }
